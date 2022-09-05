ae50712c81c3c4dd52e00e33cae6c3eae14d5483
Andreas Seeger <splash@appfros.ch>
2022-09-05 15:32:46 +0200

    debugging missing available badge every second read: replacing app loop with the one from the other project

--- fzwk-access/controller/app_loop_controller.py
+++ fzwk-access/controller/app_loop_controller.py
@@ -92,8 +92,8 @@ def app_loop():
             app.logger.debug('Badge id: %i' % badge_id)
             is_valid = db_controller.is_badge_valid(badge_id)
             if is_valid:
-                currentBadge = badge_id
-                currentPerson = person
+                # currentBadge = badge_id
+                # currentPerson = person
                 app.logger.debug('Valid: activating')
                 # db_controller.writeToLog('Activating.',
                 #                              person=self.currentPerson, badgeId=self.currentBadge)
