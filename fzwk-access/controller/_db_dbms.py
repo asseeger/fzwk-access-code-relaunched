d7679b09009a6ce470b4d5be285e4c3c11fccf09
Andreas Seeger <splash@appfros.ch>
2022-09-05 13:34:20 +0200

    commenting out debug log message

--- fzwk-access/controller/_db_dbms.py
+++ fzwk-access/controller/_db_dbms.py
@@ -36,7 +36,7 @@ def set_is_app_loop_running(is_running: bool):
 
 
 def get_is_relay_switched_on():
-    app.logger.debug('get_is_relay_switched_on(): entering')
+    # app.logger.debug('get_is_relay_switched_on(): entering')
     # with dbm.open(_dbm_store_location) as dbms:
         # is_relay_switched_on_string = dbms.get(_is_relay_switched_on_literal)
         # is_relay_switched_on_string = get_bool_from_key(_is_relay_switched_on_literal)
@@ -49,7 +49,7 @@ def get_is_relay_switched_on():
 
 
 def set_is_relay_switched_on(is_switched_on: bool):
-    # app.logger.debug(f'set_is_relay_switched_on({is_switched_on}): entering')
+    app.logger.debug(f'set_is_relay_switched_on({is_switched_on}): entering')
     # with dbm.open(_dbm_store_location, 'c') as dbms:
     #     current_app.logger.debug('writing to dbm')
     #     is_switched_on_string = str(is_switched_on)
