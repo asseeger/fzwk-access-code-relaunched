0b5c23a0701aedd86b76ccd8d448f54e95823731
Andreas Seeger <splash@appfros.ch>
2022-10-15 11:33:36 +0200

    Improved logging in /api/insertBadgePerson

--- fzwk-access/routes/api.py
+++ fzwk-access/routes/api.py
@@ -99,15 +99,15 @@ def insert_badge_person():
     if not db_controller.get_is_in_admin_mode() and not db_controller.get_is_in_insert_badge_mode():
         message = 'Der Server ist weder im Admin- noch im Badge-Insert-Modus.'
         current_app.logger.debug(message)
-        return make_response(message, 400)
+        return make_response(message, 427)
     elif not db_controller.get_is_in_admin_mode():
         message = 'Der Server ist nicht im Admin-Modus.'
         current_app.logger.debug(message)
-        return make_response(message, 400)
+        return make_response(message, 428)
     elif not db_controller.get_is_in_insert_badge_mode():
         message = 'Der Server ist nicht im Badge-Insert-Modus.'
         current_app.logger.debug(message)
-        return make_response(message, 400)
+        return make_response(message, 429)
 
     badge_id = content['badgeId']
     first_name = content['firstName']
